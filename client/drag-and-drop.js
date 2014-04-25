26d7ccb3c532927323e606dbe35f534db0fdc570
Abigail Watson <awatson1978@gmail.com>
2014-04-25 01:07:47 -0400

    whee!  got draggable() and droppable() working!

--- client/drag-and-drop.js
+++ client/drag-and-drop.js
@@ -1,17 +1,43 @@
-Template.hello.greeting = function () {
+Session.setDefault('movedElementId', null);
+Session.setDefault('mouseMoveElementId', 'Hello Dropzones!');
+Session.setDefault('mouseMoveX', 0);
+Session.setDefault('mouseMoveY', 0);
+
+Template.helloPage.greeting = function () {
   return "Welcome to drag-and-drop.";
 };
 
-Template.hello.rendered = function(){
-  $('#redPanel').draggable();
-  $('#greenPanel').draggable();
-  $('#bluePanel').draggable();
+Template.helloPage.rendered = function(){
+  $(document).mousemove(function (e) {
+     Session.set('movedElementId', e.target.id);
+     Session.set('mouseMoveX', e.clientX);
+     Session.set('mouseMoveY', e.clientY);
+     //Session.set('mouseMoveElementId', document.elementFromPoint(e.clientX,e.clientY));
+  });
+
+  $('.dragDropBlock').draggable({
+    stop: function(event, ui){
+      //console.log(document.querySelectorAll( ":hover" ));
+      console.log(document.elementFromPoint(Session.get('mouseMoveX'),Session.get('mouseMoveY')).id)
+    },
+    grid: [ 10, 10 ],
+    opacity: 0.35
+  });
+  $('.dragdropzone').droppable({
+    accept: ".dragDropBlock",
+    drop: function(){
+      $('#' + Session.get('movedElementId')).appendTo("#" + this.id);
+      $('#' + Session.get('movedElementId')).css('top', '0');
+      $('#' + Session.get('movedElementId')).css('left', '0');
+      Session.clear('movedElementId');
+    }
+  });
 }
 
-Template.hello.events({
-  'click input': function () {
-    // template data, if any, is available in 'this'
-    if (typeof console !== 'undefined')
-      console.log("You pressed the button");
+Template.helloPage.getMouseMoveId = function(){
+  if(Session.get('mouseMoveElementId')){
+    return Session.get('mouseMoveElementId');
+  }else{
+    return "...";
   }
-});
+}
